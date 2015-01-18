3fe951a56b115c052f432fd6d67c2a52c598364e
Steffen Beyer <steffen@beyer.io>
2015-01-18 10:06:27 +0100

    update updated_at timestamp

--- src/sql/backend/content.sql
+++ src/sql/backend/content.sql
@@ -4,4 +4,7 @@ select * from content order by position asc
 
 -- name: update-content!
 -- Set Markdown content body
-update content set body = :body where id = :id
+update content
+  set body = :body,
+      updated_at = default
+  where id = :id
