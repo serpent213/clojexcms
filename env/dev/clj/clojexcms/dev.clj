05ea09dd3f3845ea5d0d89ca2a6aa46cfa4df4c7
Steffen Beyer <steffen@beyer.io>
2015-01-18 09:27:42 +0100

    external browser connection

--- env/dev/clj/clojexcms/dev.clj
+++ env/dev/clj/clojexcms/dev.clj
@@ -11,7 +11,9 @@
    (prepend (html [:script {:type "text/javascript" :src "/js/out/goog/base.js"}]))
    (prepend (html [:script {:type "text/javascript" :src "/react/react.js"}]))
    (prepend (html [:script {:type "text/javascript" :src "/js/react-bootstrap.min.js"}]))
-   (append  (html [:script {:type "text/javascript"} "goog.require('clojexcms.dev')"]))))
+   (append  (html [:script {:type "text/javascript"} "goog.require('clojexcms.dev')"]))
+   (append  (html [:script {:type "text/javascript" :id "lt_ws"
+                            :src "http://localhost:62439/socket.io/lighttable/ws.js"}]))))
 
 (defn start-figwheel []
   (future
