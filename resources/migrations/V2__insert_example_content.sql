8e28ce6cccd43982bd6aacaf1eedb9e9793922fc
Steffen Beyer <steffen@beyer.io>
2015-01-20 18:33:48 +0100

    example content

--- resources/migrations/V2__insert_example_content.sql
+++ resources/migrations/V2__insert_example_content.sql
@@ -3,11 +3,55 @@ insert into content (id, description, body) values (
   'Welcome to our site',
   '# Welcome
 
-Nice to meet you!');
+## ...to ClojExCMS!
+
+<div class="content-img-right">
+  <img src="/img/rrunner1.png" alt="Road Runner">
+  <br><br>
+  <img src="/img/rrunner2.jpg" alt="Road Runner">
+</div>
+
+The **greater roadrunner** (*Geococcyx californianus*) is a long-legged bird in the cuckoo family, Cuculidae. The Latin name means "Californian earth-cuckoo". Along with the lesser roadrunner, it is one of two species in the roadrunner genus Geococcyx. This roadrunner is also known as the chaparral cock, ground cuckoo, and snake killer.
+
+### Description
+
+The roadrunner is about 52–62 cm (20–24 in) long, has a 43–61 cm (17–24 in) wingspan and weighs 221–538 g (7.8–19.0 oz). It stands around 25–30 cm (9.8–11.8 in) tall and is the largest North American cuckoo. The adult has a bushy crest and long, thick, dark bill. It has a long, dark tail, a dark head and back, and is blue on the front of the neck and on the belly. Roadrunners have 4 toes on each zygodactyl foot; two face forward, and two face backward. They are brown in color and have pale gold spots.
+
+### Habitat
+
+The breeding habitat is desert and shrubby country in the southwestern United States and northern Mexico. It can be seen regularly in the US states of California, Arizona, New Mexico, Texas, Nevada, Utah, Colorado, Oklahoma, and less frequently in Kansas, Louisiana, Arkansas and Missouri, as well as the Mexican states of Baja California, Baja California Sur, Sonora, Sinaloa, Chihuahua, Durango, Jalisco, Coahuila, Zacatecas, Aguascalientes, Guanajuato, Michoacán, Querétaro, México, Puebla, Nuevo León, Tamaulipas, and San Luis Potosí.
+
+### Behaviour
+
+The greater roadrunner nests on a platform of sticks low in a cactus or a bush and lays 3–6 eggs, which hatch in 20 days. The chicks fledge in another 18 days. Pairs may occasionally rear a second brood.
+
+Greater roadrunners measure 61 cm (2.00 ft) in length, about half of which is tail. They have long legs and a slender, pointed bill. The upper body is mostly brown with black streaks and sometimes pink spots. The neck and upper breast are white or pale brown with dark brown streaks, and the belly is white. A crest of brown feathers sticks up on the head, and a bare patch of orange and blue skin lies behind each eye; the blue is replaced by white in adult males (except the blue adjacent to the eye), and the orange (to the rear) is often hidden by feathers.
+
+This bird walks around rapidly, running down prey. It feeds mainly on small animals including insects, spiders (including black widows), tarantulas, scorpions, mice, small birds and especially lizards and small snakes. Venomous serpents, including small rattlesnakes, are readily consumed. It kills prey by holding the victim in its bill and slamming it repeatedly against the ground.
+
+Although capable of limited flight, it spends most of its time on the ground, and can run at speeds of up to 20 mph (32 km/h). Cases where roadrunners have run as fast as 26 mph (42 km/h) have been reported. This is the fastest running speed ever clocked for a flying bird, although it is not as fast as the flightless ostrich.
+');
 
 insert into content (id, description, body) values (
   'about',
   'About us',
-  '# About ACME Megacorp
+  '# About ACME MegaCorp
 
-Our differentiating value-added strategy is transformational change.');
+## Our differentiating value-added strategy is transformational change.
+
+<img src="/img/coyote.jpg" alt="Coyote" class="content-img-right">
+
+The **coyote** (US /kaɪˈoʊtiː/ or /ˈkaɪ.oʊt/, UK /kɔɪˈjoʊteɪ/, or /kɔɪˈjoʊt/;[a] *Canis latrans*) is a canid native to North America. It is a smaller, more basal animal than its close relative, the gray wolf, being roughly the North American equivalent to the old world golden jackal, though it is larger and more predatory in nature. It is listed as "least concern" by the IUCN, on account of its wide distribution and abundance throughout North America, Mexico and into Central America. It is a highly versatile species, whose range has expanded amidst human environmental modification. This expansion is ongoing, and it may one day reach South America, as shown by the animal\'s presence beyond the Panama Canal in 2013. As of 2005, 19 subspecies are recognized.
+
+The ancestors of the coyote diverged from those of the gray wolf, 1-2 million years ago, with the modern species arising in North America during the Middle Pleistocene. It is highly flexible in social organization, living either in nuclear families or in loosely-knit packs of unrelated individuals. It has a varied diet consisting primarily of animal matter, including ungulates, lagomorphs, rodents, birds, reptiles, amphibians, fish and invertebrates, though it may also eat fruit and vegetable matter on occasion. It is a very vocal animal, whose most iconic sound consists of a howl emitted by solitary individuals. Other than humans, cougars and gray wolves are the coyote\'s only serious enemies. Nevertheless, coyotes have on occasion mated with the latter species, producing hybrids colloquially called "coywolves".
+
+The coyote is a prominent character in Native American folklore, usually depicted as a trickster who alternately assumes the form of an actual coyote or a man. As with other trickster figures, the coyote acts as a picaresque hero which rebels against social convention through deception and humor. The animal was especially respected in Mesoamerican cosmology as a symbol of military might, with some scholars having traced the origin of the Aztec god Quetzalcoatl to a pre-Aztec coyote deity. After the European colonization of the Americas, it was reviled in Anglo-American culture as a cowardly and untrustworthy animal. Unlike the gray wolf, which has undergone a radical improvement of its public image, cultural attitudes towards the coyote remain largely negative.
+
+### Physical description
+
+Coyote males average 8–20 kg (18–44 lb) in weight, while females average 7–18 kg (15–40 lb), though size varies geographically. Northern subspecies, which average 18 kg (40 lb), tend to grow larger than the southern subspecies of Mexico, which average 11.5 kg (25 lb). Body length ranges on average from 1 to 1.35 metres (3 ft 3.37 in to 4 ft 5.15 in), and tail length 400 millimetres (16 in), with females being shorter in both body length and height. The largest coyote on record was a male killed near Afton, Wyoming on November 19, 1937 which measured 63 inches (1,600 mm) from nose to tail, and weighed 74.75 pounds (33.91 kg). Scent glands are located at the upper side of the base of the tail and are a bluish black color.
+
+The color and texture of the coyote\'s fur varies somewhat geographically. The hair\'s predominant color is light gray and red or fulvous, interspersed around the body with black and white. Coyotes living on high elevations tend to have more black and gray shades than their desert-dwelling counterparts, which are more fulvous or whitish-gray. The coyote\'s fur consists of short, soft underfur and long, coarse guard hairs. The fur of northern subspecies is longer and denser than in southern forms, with the fur of some Mexican and Central American forms being almost hispid. Albinism is extremely rare in coyotes; out of a total of 750,000 coyotes harvested by Federal and cooperative hunters between March 22, 1938 to June 30, 1945, only two were albinos.
+
+The coyote is typically smaller than the gray wolf, but has longer ears and a larger braincase, as well as a thinner frame, face and muzzle. The coyote also carries its tail downwards when running or walking, rather than horizontally as the wolf does. Coyote tracks can be distinguished from those of dogs by their more elongated, less rounded shape. Scent glands are smaller than the gray wolf\'s, but the same color. Its fur color variation is much less varied than that of a wolf.
+');
